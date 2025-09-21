616ff240c316abd0ef074b4a4527a0c318af5ae0
DenisKolaida <koladadenis07p@gmail.com>
2025-09-21 18:56:54 +0300

    3.2.16

--- app/main.py
+++ app/main.py
@@ -1,19 +1,32 @@
 # main.py
 from fastapi import FastAPI, Response, Cookie, HTTPException
 from itsdangerous import Signer, BadSignature
-from uuid import uuid4
-
 import app.db_scripts as dbs
 from app.models import UserData
+from datetime import date, datetime
+
+
+COOKIE_NAME = "session_token"
+SECRET_KEY = 'HelloWorld'
+SESSION_LIFETIME = 5 * 60
 
 
 app = FastAPI()
-signer = Signer("HelloWorld")
+signer = Signer(SECRET_KEY)
+
+
+def check_cookie_time(current_time: int, cookie_time: int):
+    difference = current_time - cookie_time
+    if difference >= SESSION_LIFETIME or difference <= 0:
+        return "bad"
+    elif difference >= 3*60:
+        return "change"
+    return "leave"
 
 
 @app.get("/delck")
 async def delete_cookie(response: Response):
-    response.delete_cookie(key="session_token")
+    response.delete_cookie(key=COOKIE_NAME)
     return {"message": "Cookie has been set!"}
 
 
@@ -26,25 +39,34 @@ async def sign_up(user_data: UserData):
 @app.post("/login")
 async def login(user_data: UserData, response: Response):
     uuid = await dbs.login_user(user_data.username, user_data.password)
+    uuid = uuid[0] + f".{int(datetime.today().timestamp())}"
     if uuid:
-        signed_uuid = signer.sign(uuid[0]).decode()
-        response.set_cookie(key="session_token", value=signed_uuid, httponly=True, max_age=3600)
+        signed = signer.sign(uuid).decode()
+        response.set_cookie(key=COOKIE_NAME, value=signed, httponly=True, max_age=SESSION_LIFETIME)
         return {"message": "Logged successfully."}
     return {"message": "Invalid username or password."}
 
 
 @app.get("/user")
-async def user(session_token: str | None = Cookie(default=None)):
+async def user(response: Response, session_token: str | None = Cookie(default=None)):
     try:
-        uuid = signer.unsign(session_token).decode()
+        # If data or user_data is None, it raises TypeError
+        data = signer.unsign(session_token).decode().split('.')
+        uuid = data[0]
+        timestamp = int(data[1])
+        current_timestamp = int(datetime.today().timestamp())
+
+        match check_cookie_time(current_timestamp, timestamp):
+            case "bad":
+                None[0] # Causes TypeError (then 401) which is not logging
+            case "change":
+                response.set_cookie(key=COOKIE_NAME, value=signer.sign(uuid+f".{current_timestamp}").decode(), httponly=True, max_age=SESSION_LIFETIME)
+            case "leave":
+                ... # It's just doing nothing :)
+
         user_data = await dbs.find_user_by_uuid(uuid)
-        if user_data:
-            return {"message":{"username": user_data[0][0], "password": user_data[0][1]}}
-        else:
-            None[0]
+        return {"message":{"username": user_data[0][0], "password": user_data[0][1]}}
     except Exception as e:
-        if not type(e) == TypeError:
+        if type(e) != TypeError:
             print(f"[!] ERROR: {e}")
         raise HTTPException(status_code=401)
-    
-    
